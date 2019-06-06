68fa0040df39e40ae34184e70bab2bab0839842b
juanblas09 <jbtschopp@gmail.com>
2019-06-06 10:39:51 -0300

    test

--- _includes/jumbotron.php
+++ _includes/jumbotron.php
@@ -1,4 +1,4 @@
 <div class="jumbotron">
-    <h1 class="display-4">Bienvenido/a a MovoBus</h1>
+    <h1 class="display-3">Bienvenido/a a MovoBus</h1>
     <p class="lead">La app impulsada por FM Comunicarte para consulta de horarios de colectivos en la región</p>
 </div>
