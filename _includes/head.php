4e973eb695f0dacc8a60ef7ecf9d0ffbde1347a4
juanblas09 <jbtschopp@gmail.com>
2019-06-06 00:09:12 -0300

    update

--- _includes/head.php
+++ _includes/head.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>MovoBus</title>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
     <script defer src="https://use.fontawesome.com/releases/v5.8.2/js/all.js" 
       integrity="sha384-DJ25uNYET2XCl5ZF++U8eNxPWqcKohUUBUpKGlNLMchM7q4Wjg2CUpjHLaL8yYPH" 
